4a634b85808b7612094f01adfa58cea26b594a1c
Hardeep Patnaik <harrypatnaik1@gmail.com>
2024-03-13 20:36:21 +0530

    added some more information on callbacks in states_01

--- states_01/src/App.jsx
+++ states_01/src/App.jsx
@@ -2,19 +2,34 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(15);
 
   function AddCount() {
     setCount(count + 1);
   }
 
+  //  this function doesnt increase the count by 2
+
+  // function AddCount() {
+  //   setCount(count + 1);
+  //   setCount(count + 1);
+  // }
+
+  // To really increase the count by 2 we use callbacks
+
+  // const AddCount = () => {
+  //   setCount((PrevCount) => PrevCount + 1);
+  //   setCount((PrevCount) => PrevCount + 1);
+  // };
+
   function SubCount() {
     setCount(count - 1);
   }
+
   return (
     <>
       <h1>count : {count}</h1>
-      <button onClick={AddCount}>ADD</button>{" "}
+      <button onClick={AddCount}>ADD</button>
       <button onClick={SubCount}>SUB</button>
     </>
   );
